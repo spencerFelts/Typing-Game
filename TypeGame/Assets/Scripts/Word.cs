9f1ccba6185c0aafe2fcccb0c4d42f0328ff7e4a
spencerFelts <71102799+spencerFelts@users.noreply.github.com>
2021-02-18 13:39:57 -0600

    Basic Typing Game

---
+++ TypeGame/Assets/Scripts/Word.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Word 
+{
+    public string word;
+    private int typeIndex;
+
+    WordDisplay display;
+
+    public Word (string _word, WordDisplay _display)
+    {
+        word = _word;
+        typeIndex = 0;
+
+        display = _display;
+        display.SetWord(word);
+    }
+
+    public char GetNextLetter()
+    {
+        return word[typeIndex];
+    }
+
+    public void TypeLetter()
+    {
+        typeIndex++;
+        //Remove the letter on screen
+        display.RemoveLetter();
+    }
+
+    public bool WordTyped()
+    {
+        bool wordTyped = (typeIndex >= word.Length);
+        if (wordTyped)
+        {
+            //Remove the word on screen
+            display.RemoveWord();
+        }
+        return wordTyped;
+    }
+}
---
+++ TypeGame/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+    private static string[] wordList = { "school", "university", "functions", "objects", "classes", "games", "array", "programming", "coding", "computer"};
+
+    public static string GetRandomWord()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    }
+}
